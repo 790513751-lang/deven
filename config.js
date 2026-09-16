bbf49cc82e889b5977da8f8eeda9baac7c9a3c01
790513751-lang <790513751@qq.com>
2026-09-16 18:40:06 +0800

    发布移动端引导页

---
+++ config.js
@@ -0,0 +1,15 @@
+window.GUIDE_CONFIG = {
+  brandName: "TG 上手指南",
+  networkUrl: "https://ktmcloud.vip/#/register?code=TEAfip3S",
+  channelUrl: "https://fanshen-trading-park.vercel.app/",
+  apple: {
+    createUrl: "https://account.apple.com/",
+    tutorialUrl: "https://support.apple.com/zh-cn/108647"
+  },
+  telegram: {
+    iphoneUrl: "https://apps.apple.com/us/app/telegram-messenger/id686449807",
+    androidPageUrl: "https://telegram.org/android",
+    androidApkUrl: "https://telegram.org/dl/android/apk"
+  }
+};
+
